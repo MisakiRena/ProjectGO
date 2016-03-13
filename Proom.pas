1934b255628b63b3ea8ec6ea597d9aa93b5b15cd
MisakiRena <AnotherMisaki545@gmail.com>
2016-03-13 11:05:36 +0800

    框架

---
+++ Proom.pas
@@ -0,0 +1,26 @@
+unit Proom;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, RzPanel;
+
+type
+  TForm2 = class(TForm)
+    RzPanel1: TRzPanel;
+    RzPanel2: TRzPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
