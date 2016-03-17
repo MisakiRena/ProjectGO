3833aa9afa980a135a1c0e580e4978d1caef4625
MisakiRena <AnotherMisaki545@gmail.com>
2016-03-17 11:36:54 +0800

    棋盤生成更新

--- Proom.pas
+++ Proom.pas
@@ -13,7 +13,7 @@ type
     Button1: TButton;
     TRZPL: TRzPanel;
     image1: TImage;
-    procedure Draw(ti:TImage);
+    procedure DrawBoardFrame(ti:TImage);
     procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
@@ -32,22 +32,29 @@ implementation
 
 procedure TForm2.Button1Click(Sender: TObject);
 begin
-  Draw(Image1);  
+  DrawBoardFrame(Image1);
 end;
 
-procedure TForm2.Draw(ti:TImage);
+procedure TForm2.DrawBoardFrame(ti:TImage);
 Var
-I:Integer;
+I,Bh,Bm,Bn,Bp:Integer;
+trz:TRzPanel;
 begin
 ti.Picture.Bitmap.LoadFromFile('D:\200772095941245_2.bmp');
-
+trz:=TRzPanel(ti.Parent);
+Bh:=trz.Height;     //取得父元件大小
+Bp:=19*Bh div 20;   //繪製棋盤大小(19格+1格預留邊框)
+Bm:=Bp div 19;
+Bn:=Bh-Bm;
 For I := 0 to 18 do
   begin
-  if (I=0)or(I=18) then ti.Canvas.Pen.Width:=2 else ti.Canvas.Pen.Width:=1;
-  ti.Canvas.MoveTo(20,I*32+20); //20.20  起,148 I320
-  ti.Canvas.LineTo(596,I*32+20);//596.20 終,148
-  ti.Canvas.MoveTo(I*32+20,20); //20.20  148,起
-  ti.Canvas.LineTo(I*32+20,596);//20.596 148,終
+  if (I=0)or(I=18) then ti.Canvas.Pen.Width:=3 else
+    if (I=3)or(I=9)or(I=15)  then  ti.Canvas.Pen.Width:=2
+    else  ti.Canvas.Pen.Width:=1;
+  ti.Canvas.MoveTo(Bm,Bm+I*Bm);        //20.20  起,148 I320
+  ti.Canvas.LineTo(Bn,Bm+I*Bm);     //596.20 終,148
+  ti.Canvas.MoveTo(Bm+I*Bm,Bm);     //20.20  148,起
+  ti.Canvas.LineTo(Bm+I*Bm,Bn);     //20.596 148,終
   end;
 
 end;
--- preServer/Unit3.dfm
+++ preServer/Unit3.dfm
@@ -2,8 +2,8 @@ object Form3: TForm3
   Left = 0
   Top = 0
   Caption = 'Form3'
-  ClientHeight = 201
-  ClientWidth = 447
+  ClientHeight = 299
+  ClientWidth = 596
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
